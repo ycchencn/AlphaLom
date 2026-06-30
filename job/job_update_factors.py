394a6f80f15278290e8462427b5fc7765ee55aad
Yc Chen <yccheni@163.com>
2026-06-30 16:02:09 +0800

    删除因子计算

--- job/job_update_factors.py
+++ job/job_update_factors.py
@@ -4,18 +4,12 @@
  * Copyright (c) 2025 yccheni@163.com. All rights reserved.
 """
 
-import akshare as ak
 import pandas as pd
-import time
-from service import IndexConstituentsService, FactorValueService, StockService, FactorCalService
-from utils.common import df_cache, logger, get_date_by_n, get_today
+from service import FactorValueService, StockService, FactorCalService
+from utils.common import logger, get_date_by_n, get_today
 from utils.financial_data import INDICATOR_NAME_MAP
 from service import JobService
 
-# 使用
-@df_cache()
-def get_financial_df(stock: str):
-    return ak.stock_financial_abstract(symbol=stock)
 
 def convert_to_factor_records(stock_code: str, formatted_dict: dict):
     """
@@ -36,6 +30,7 @@ def convert_to_factor_records(stock_code: str, formatted_dict: dict):
                 'value': value
             })
     return records
+
 
 def format_financial_data(df):
     result = {}
@@ -61,67 +56,8 @@ def format_financial_data(df):
         result[indicator] = time_series
     return result
 
-def update_financial_factor(stock_code):
-
-    factor_ext = FactorValueService.get_stock_factor_series(stock_code, factor_name='roe')
-
-    if len(factor_ext) > 10:
-        print(f'{stock_code}, factor exist, len(records): {len(factor_ext)}, return')
-        return
-
-    df = get_financial_df(stock=stock_code)
-
-    # 数据格式化
-    formated_df = format_financial_data(df)
-
-    factor_dict = convert_to_factor_records(stock_code=stock_code, formatted_dict=formated_df)
-
-    records = []
-    for i in factor_dict:
-        # print(stock_code, i['report_date'], i['factor_name'], i['value'])
-        records.append({
-            'ticker': stock_code,
-            'trade_date': i['report_date'],
-            'factor_name': i['factor_name'],
-            'value': i['value']
-        })
-
-    FactorValueService.bulk_create(records)
-
-    print(f'{stock_code}, factor added, len(records): {len(records)}')
-
-    return True
-
-
-def job_update_financial_factors_by_index_constituents(index_code=None):
-
-    if index_code is None:
-        return
-
-    # 获取中证500指数成分股，刷新财务数据因子
-    _index_stock_cons = IndexConstituentsService.get_by_index_code(index_code=index_code, page_size=1000)
-
-    for stock in _index_stock_cons['items']:
-
-        stock_code = stock.get('stock_code')
-
-        update_financial_factor(stock_code=stock_code)
-
-
-def job_update_financial_factors_all():
-
-    stocks = StockService.search_stocks(securities_type='stock', monitoring=1, per_page=10000)
-
-    # 循环对个股进行每日挖掘
-    for stock in stocks:
-        if stock.get('market') != 'cn':
-            continue
-        stock_code = stock.get('symbol')
-        if update_financial_factor(stock_code=stock_code):
-            time.sleep(5)
 
 def job_update_stock_factor_daily():
-
     # 判断交易日
     if FactorValueService.is_trading_day() is False:
         return
@@ -130,7 +66,6 @@ def job_update_stock_factor_daily():
 
     # 循环对个股进行每日挖掘
     for stock in stocks:
-
         JobService.send_job({
             'job_func': 'job_update_stock_factor',
             'job_args': {
@@ -140,11 +75,13 @@ def job_update_stock_factor_daily():
             }
         })
 
+
 def job_update_stock_factor_daily_all():
     stocks = StockService.search_stocks(securities_type='stock', monitoring=1, per_page=10000)
     # 循环对个股进行每日挖掘
     for stock in stocks:
         job_update_stock_factor(stock_code=stock['symbol'], save_last=True, time_period=-360)
+
 
 def job_update_stock_factor(stock_code, trade_date=None, save_last=False, time_period=-360):
     """
@@ -170,11 +107,11 @@ def job_update_stock_factor(stock_code, trade_date=None, save_last=False, time_p
         FactorValueService.delete_by_ticker(stock_code=stock_code)
         FactorCalService.save_factor_records_to_db(stock_code, factors)
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     # job_update_financial_factors_by_index_constituents(index_code='000016')
     # df = get_financial_df(stock='600111')
 
-    job_update_stock_factor(stock_code='300750', save_last=False, time_period=-365)
+    # job_update_stock_factor(stock_code='300750', save_last=False, time_period=-365)
 
-    # job_update_stock_factor_daily_all()
+    job_update_stock_factor_daily_all()
