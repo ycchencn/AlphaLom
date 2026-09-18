955cbabe1a8a8bf973300e2d61080bbd34796338
ycchen <yccheni@163.com>
2026-09-18 19:47:14 +0800

    chore: uvicorn workers 默认值由 3 改为 1

--- run_fastapi.py
+++ run_fastapi.py
@@ -99,5 +99,5 @@ if __name__ == '__main__':
         host='0.0.0.0',
         port=8080,
         reload=(env=='dev'),
-        workers=int(os.getenv('WEB_WORKERS', '3'))
+        workers=int(os.getenv('WEB_WORKERS', '1')),
     )
